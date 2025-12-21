9ae0e59f6ab9c31126b1bd73b93df686804b2bb7
yasmramos <yasmramos95@gmail.com>
2025-12-22 03:12:41 +0800

    Remove duplicate jakarta.persistence dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,7 +7,7 @@ module io.github.yasmramos.axia {
     requires io.ebean.core;
     requires io.ebean.datasource;
     requires java.sql;
-    requires jakarta.persistence;
+
     
     requires org.slf4j;
     requires com.zaxxer.hikari;
